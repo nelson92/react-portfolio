2609d9fa51f48d423fc0bfdeb3b9be2daf58a536
nelson509 <35314794+nelson509@users.noreply.github.com>
2023-01-14 12:07:15 -0800

    updated some components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
+import Portfolio from './components/portfolio';
+import Navigation from './components/navigation';
+import About_me from './components/about_me';
+import Contact from './components/contact';
+import Project from './components/project';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header> 
+
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/contact/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+function Contact() {
+ return (
+    <div>
+        Contact
+    </div>
+
+ )
+
+
+}
+
+export default Contact;

4bce41b9ca1bd7cab3241c1f7aa0721f94ddda99
nelson509 <35314794+nelson509@users.noreply.github.com>
2023-01-16 15:49:27 -0800

    worked on footer and about me components

--- src/components/about_me/index.js
+++ src/components/about_me/index.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+
 
 
 function AboutMe() {
-    
 
+   return (
+    <div>
+      <p className="content is-medium">System Analyst and Full Stack Web Development Student</p>
+      <hr />
+      {/* TODO add link to picture */}
+      {/* <img className="my-pic" src={process.env.PUBLIC_URL + '/img/profile.jpg'} alt="Richard Nelson"/> */}
+      <p className="content mt-4">
+        I'm Richard and I'm currently a Systems Analyst at a Seattle-based health care start up. 
+        I've got 7 years of experience of working at various positions at a tech company and have worked my way up from phone support to associate software developer.
+      </p>
+      
+    </div>
+   ) 
+};
 
-}
+export default AboutMe;
---
+++ src/components/footer/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// TODO import icons from separate folder
+
+
+function Footer() {
+    
+    return (
+        <footer className="footer">
+      <div>
+      <p>
+          <strong>React Portfolio</strong> by {" "}
+          <a href="https://github.com/nelson92" target="_blank" rel="noreferrer">
+            Richard Nelson
+          </a>
+          
+          <hr />
+          <a href="https://www.linkedin.com/in/richardtnelson/"
+            target="_blank" rel="noreferrer"> LinkedIn
+          </a>{" "}
+         
+          
+        </p>
+      </div>
+    </footer>
+
+  
+    )
+
+};
+
+export default Footer;
+
